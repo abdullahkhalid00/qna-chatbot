cdcb4feff1afc8215473dacc55edc499330d9e04
Abdullah Khalid <iabdulahk@gmail.com>
2023-11-03 18:08:34 +0100

    Added pdf text extraction function

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 import streamlit as st
 
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
 
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
 
 def main():
     load_dotenv()
@@ -12,8 +21,13 @@ def main():
 
     with st.sidebar:
         st.subheader("Your documents")
-        st.file_uploader("Upload your PDFs here and click 'Process'", accept_multiple_files=True)
-        st.button("Process")
+        pdf_docs = st.file_uploader("Upload your PDFs here and click 'Process'", accept_multiple_files=True)
+        
+        if st.button("Process"):
+            with st.spinner("Processing"):
+
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
 
 
 if __name__ == "__main__":
