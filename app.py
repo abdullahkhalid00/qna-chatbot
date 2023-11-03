b06421d81e38481cd6687d64b34a036bfc81507c
Abdullah Khalid <iabdulahk@gmail.com>
2023-11-03 18:14:14 +0100

    Created vectorstore using FAISS and OpenAI

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
 
 
 def get_pdf_text(pdf_docs):
@@ -22,6 +24,11 @@ def get_text_chunks(text):
     )
     chunks = text_splitter.split_text(text)
     return chunks
+
+def get_vector_store(text_chunks):
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vector_store = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vector_store
 
 
 def main():
@@ -43,6 +50,9 @@ def main():
 
                 # get text chunks
                 text_chunks = get_text_chunks(raw_text)
+                
+                # get vector store using embeddings
+                vector_store = get_vector_store(text_chunks)
 
 
 if __name__ == "__main__":
