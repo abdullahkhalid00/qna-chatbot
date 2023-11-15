46d73decb84ba35df8974cdc79bec0488c39779f
Abdullah Khalid <iabdulahk@gmail.com>
2023-11-15 19:23:51 +0100

    Converted speech to text with whisper-1

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+import os
 import streamlit as st
 
 from dotenv import load_dotenv
-from PyPDF2 import PdfReader
+from openai import OpenAI
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
@@ -10,13 +11,14 @@ from langchain.vectorstores import FAISS
 from langchain.chains import ConversationalRetrievalChain
 
 
-def get_pdf_text(pdf_docs):
-    text = ""
-    for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-    return text
+def get_transcript(audio_file):
+    client = OpenAI()
+    with open(audio_file, 'rb') as audio_file:
+        transcript = client.audio.transcriptions.create(
+        model="whisper-1", 
+        file=audio_file
+        )
+    return transcript.text
 
 def get_text_chunks(text):
     text_splitter = CharacterTextSplitter(
@@ -71,23 +73,25 @@ def main():
     )
     st.header(body="Google Meet AI Assistant 🤖")
 
-    user_question = st.chat_input(placeholder="Ask a question about your meeting transcript(s).")
+    user_question = st.chat_input(placeholder="Ask a question about your meeting.")
     if user_question:
         handle_user_input(user_question)
 
     with st.sidebar:
-        st.subheader("Your transcript(s)")
-        pdf_docs = st.file_uploader("Upload your transcript PDFs here and click 'Process'", accept_multiple_files=True)
+        st.subheader("Your meeting(s)")
+        audio_file = st.file_uploader("Upload your meeting audio here and click 'Process'", accept_multiple_files=False, type=["mp3", "wav"])
 
-        if pdf_docs:
+        if audio_file:
+            filename = os.path.join("./audio", audio_file.name)
+            
             if st.button("Process"):
                 with st.spinner("Processing"):
 
-                    # get pdf text
-                    raw_text = get_pdf_text(pdf_docs)
+                    # get meeting transcript
+                    transcript = get_transcript(audio_file=filename)
 
                     # get text chunks
-                    text_chunks = get_text_chunks(raw_text)
+                    text_chunks = get_text_chunks(transcript)
                     
                     # get vector store using embeddings
                     vector_store = get_vector_store(text_chunks)
